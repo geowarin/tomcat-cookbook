78fd61a3602ce07efb8c4962eb3750332df65dee
geowarin <geowarin@gmail.com>
2014-12-28 22:13:52 +0100

    Inject working

--- src/main/frontend/gulpfile.js
+++ src/main/frontend/gulpfile.js
@@ -1,19 +1,17 @@
 var gulp = require('gulp'),
-    inject = require('gulp-inject'),
+    $ = require('gulp-load-plugins')(),
     bowerFiles = require('main-bower-files'),
     del = require('del'),
-    rename = require('gulp-rename'),
     runSequence = require('run-sequence'),
     path = require('path');
 
-
 var app = {
     js: [
-        'src/js/models/*.js',
-        'src/js/collections/*.js',
-        'src/js/routers/*.js',
-        'src/js/views/*.js',
-        'src/js/app.js'
+        'js/models/*.js',
+        'js/collections/*.js',
+        'js/routers/*.js',
+        'js/views/*.js',
+        'js/app.js'
     ],
     buildDir: 'build'
 };
@@ -28,32 +26,36 @@ gulp.task('appJs', function () {
 });
 
 gulp.task('vendor', function () {
-    console.log(bowerFiles().map(function(p) {
-        return path.relative('bower_components', p);
-    }));
-
-    return gulp.src(bowerFiles(), { base: 'bower_components' })
+    return gulp.src(bowerFiles(), {base: 'bower_components'})
         .pipe(gulp.dest(app.buildDir + '/vendor'));
 });
 
-//gulp.task('processIndex', function () {
-//    return gulp.src('src/html/index.html')
-//        .pipe(gulp.dest('../webapp'));
-//});
-//
-//gulp.task('injectIntoIndex', ['processIndex'], function () {
-//    return gulp.src('../webapp/index.html')
-//        .pipe(inject(
-//            gulp.src(bowerFiles({includeDev: true}), {read: false}),
-//            {name: 'bower', relative: true}
-//        ))
-//        .pipe(inject(
-//            gulp.src(app.src, {read: false}),
-//            {name: 'all', relative: true}
-//        ))
-//        .pipe(gulp.dest('../webapp'));
-//});
+gulp.task('processIndex', function () {
+    return gulp.src('src/html/index.html')
+        .pipe(gulp.dest(app.buildDir));
+});
 
-gulp.task('default', function() {
-    return runSequence('clean', ['appJs', 'vendor']);
+gulp.task('injectIntoIndex', ['processIndex'], function () {
+    var vendors = bowerFiles().map(function (p) {
+        return 'vendor/' + path.relative('bower_components', p);
+    });
+    return gulp.src(app.buildDir + '/index.html')
+        .pipe($.inject(
+            gulp.src(vendors, {read: false, cwd: app.buildDir}),
+            {name: 'bower', relative: true}
+        ))
+        .pipe($.inject(
+            gulp.src(app.js, {read: false, cwd: app.buildDir}),
+            {name: 'all', relative: true}
+        ))
+        .pipe(gulp.dest(app.buildDir));
+});
+
+gulp.task('moveToWebapp', function () {
+    return gulp.src(['build/**'])
+        .pipe(gulp.dest('../webapp'))
+});
+
+gulp.task('default', function () {
+    return runSequence('clean', ['appJs', 'vendor'], 'injectIntoIndex', 'moveToWebapp');
 });
