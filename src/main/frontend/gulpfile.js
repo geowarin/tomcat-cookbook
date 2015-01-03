2ac1335e01a851eb9ce0a186e8ae23e89a91bb0d
geowarin <geowarin@gmail.com>
2015-01-03 17:48:53 +0100

    Done

--- src/main/frontend/gulpfile.js
+++ src/main/frontend/gulpfile.js
@@ -10,75 +10,73 @@ var prod = !!argv.prod; // true if --prod flag is used
 
 var paths = {
     js: [
-        'src/js/models//*.js',
-        'src/js/collections/*.js',
-        'src/js/routers/*.js',
-        'src/js/views/*.js',
-        'src/js/app.js'
+        'js/models/*.js',
+        'js/collections/*.js',
+        'js/routers/*.js',
+        'js/views/*.js',
+        'js/app.js'
     ],
-    build : {
-        base: 'build',
-        js: 'build/js',
-        vendor: 'build/vendor',
-        index: 'build/index.html'
+    build: {
+        base: '../dist'
     },
-    webapp : {
-        base: '../webapp',
-        js: '../webapp/js',
-        vendor: '../webapp/vendor',
-        index: '../webapp/index.html'
+    webapp: {
+        base: '../webapp'
     }
 };
 
+var dest = prod ? paths.build.base : paths.webapp.base;
+
 gulp.task('clean', function (cb) {
-    return del([paths.build.base, paths.webapp.js, paths.webapp.vendor, paths.webapp.index], {force: true}, cb);
+    return del([paths.build.base], {force: true}, cb);
 });
 
-gulp.task('moveIndex', function () {
-    return gulp.src('src/html/index.html')
-        .pipe(gulp.dest(paths.build.base));
-});
-
-gulp.task('inject', ['moveIndex'], function () {
-    var vendor = gulp.src(bowerFiles(), {base: 'bower_components'})
+var index = function () {
+    if (!prod) {
+        return gulp.src(paths.webapp.base + '/index.html')
+    }
+    return gulp.src(paths.webapp.base + '/index.html')
+        .pipe(gulp.dest(dest));
+};
+var js = function () {
+    if (!prod) {
+        return gulp.src(paths.js, {cwd: '../webapp'})
+    }
+    return gulp.src(paths.js, {cwd: '../webapp'})
+        .pipe($.if('*.js', minifyJs('app.min.js')()))
+        .pipe(gulp.dest(dest));
+};
+var vendor = function () {
+    if (!prod) {
+        return gulp.src(bowerFiles())
+    }
+    return gulp.src(bowerFiles())
         .pipe($.if('*.js', minifyJs('vendor.min.js')()))
         .pipe($.if('*.css', minifyCss('vendor.min.css')()))
-        .pipe(gulp.dest(paths.build.vendor));
+        .pipe(gulp.dest(dest));
+};
 
-    var js = gulp.src(paths.js, {base: 'src'})
-        .pipe($.if('*.js', minifyJs('app.min.js')()))
-        .pipe(gulp.dest(paths.build.base));
-
-    return gulp.src(paths.build.index)
-        .pipe($.inject(vendor, {name: 'bower', relative: true}))
-        .pipe($.inject(js, {name: 'all', relative: true}))
-        .pipe(gulp.dest(paths.build.base));
+gulp.task('inject', function () {
+    return index()
+        .pipe($.inject(vendor(), {name: 'bower', relative: true}))
+        .pipe($.inject(js(), {name: 'all', relative: true}))
+        .pipe(gulp.dest(dest));
 });
 
 var minifyJs = function (destFile) {
     return lazypipe()
         .pipe(function () {
-            return $.if(prod, $.concat(destFile));
+            return $.concat(destFile);
         })
-        .pipe(function () {
-            return $.if(prod, $.uglify());
-        });
+        .pipe($.uglify)
 };
 var minifyCss = function (destFile) {
     return lazypipe()
         .pipe(function () {
-            return $.if(prod, $.concat(destFile));
+            return $.concat(destFile);
         })
-        .pipe(function () {
-            return $.if(prod, $.csso());
-        });
+        .pipe($.csso);
 };
 
-gulp.task('moveToWebapp', function () {
-    return gulp.src(['build/**'])
-        .pipe(gulp.dest(paths.webapp.base))
-});
-
 gulp.task('default', function () {
-    return runSequence('clean', 'inject', 'moveToWebapp');
+    return runSequence('clean', 'inject');
 });
